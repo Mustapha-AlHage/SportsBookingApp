518301d7a64d91763cd4d3b51d624177012d4df0
Mustapha <123622849+Mustapha-AlHage@users.noreply.github.com>
2023-06-10 14:31:31 +0300

    ----

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {useEffect} from 'react';
 
-import LogIn from './src/components/LogIn';
-import Stadium from './src/components/Stadium';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function App() {
-  useEffect(() => {
-    console.log('hello');
-  }, []);
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <>
-          {/* <Stadium /> */}
-          <LogIn />
-        </>
+        <></>
       </NavigationContainer>
     </SafeAreaProvider>
   );
@@ -39,3 +30,20 @@ export default function App() {
 //     </View>
 //   );
 // };
+
+// screenOptions={({route}) => ({
+//   tabBarIcon: ({focused, color, size}) => {
+//     let iconName;
+
+//     if (route.name === 'Stadiums') {
+//       iconName = focused ? 'stadium' : '';
+//     } else if (route.name === 'Log') {
+//       iconName = focused ? 'ios-list' : 'ios-list-outline';
+//     }
+
+//     // You can return any component that you like here!
+//     return <Icon name={iconName} size={size} color={color} />;
+//   },
+//   tabBarActiveTintColor: 'tomato',
+//   tabBarInactiveTintColor: 'gray',
+// })}
