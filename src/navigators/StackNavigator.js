59588d0f60f6ca37c2bd082a48e4b14f589ff32c
layalamm <123622758+layalamm@users.noreply.github.com>
2023-06-24 12:37:05 +0300

    adding help and about us

--- src/navigators/StackNavigator.js
+++ src/navigators/StackNavigator.js
@@ -5,15 +5,49 @@ import {StadiumStack} from './TabNavigator';
 import StadiumDetails from '../components/StadiumDetails';
 //import Profile from '../components/Profile';
 import NewProfile from '../components/NewProfile';
-
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const Tab = createBottomTabNavigator();
 
 export default function StackNavigator() {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="Stadiums" component={StadiumStack} />
-      <Tab.Screen name="Reservations" component={StadiumStack} />
-      <Tab.Screen name="NewProfile" component={NewProfile} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: '#053857',
+          height: 55,
+          // borderTopRightRadius: 20,
+          // borderTopLeftRadius: 20,
+        },
+        headerShown: false,
+      }}>
+      <Tab.Screen
+        name="Stadiums"
+        component={StadiumStack}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="stadium-variant"
+              color={'white'}
+              size={35}
+            />
+          ),
+        }}
+      />
+
+      {/* <Tab.Screen name="Reservations" component={StadiumStack} /> */}
+      <Tab.Screen
+        name="NewProfile"
+        component={NewProfile}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color, size}) => (
+            <FontAwesome name="user" color={'white'} size={35} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
